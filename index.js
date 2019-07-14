2e0d5c8af37f1129e5e0d8789ed6028eff341025
Carl Ortutay <carlorty@gmail.com>
2019-07-13 20:26:19 -0400

    added JS to make input form functional

---
+++ index.js
@@ -0,0 +1,20 @@
+function addItemHandler() {
+  $("#js-shopping-list-form").submit(function(event) {
+    event.preventDefault();
+    let addItem = $("#js-shopping-list-form").find('input').val();
+    $('.shopping-list').append(`
+    <li>
+      <span class="shopping-item">${addItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+    $('#shopping-list-entry').val('')
+  });
+};
+
